71ecba3bf7ee495ca7a9c2933b668bae3832cf03
Dominik Soczewka <soczewka.dominik@gmail.com>
2021-10-25 14:32:31 +0200

    Remove obsolete files

--- src/components/Error/Error.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Error.css';
-
-const Error = () => {
-  return <div className="Error">Error occurred! Try refreshing the page.</div>
-};
-
-export default Error;
